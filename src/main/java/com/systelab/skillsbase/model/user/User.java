78e6631abe15f195d29103ddc0f1fc0c30e1342a
alfonsserra <alfonsoserra@gmail.com>
2018-06-05 22:06:35 +0200

    Do not send skillsAssessment

--- src/main/java/com/systelab/skillsbase/model/user/User.java
+++ src/main/java/com/systelab/skillsbase/model/user/User.java
@@ -1,8 +1,10 @@
 package com.systelab.skillsbase.model.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.systelab.skillsbase.model.skill.SkillAssessment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -11,11 +13,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
+@XmlRootElement
+
 @Entity
 @Table(name = "users")
 @Data
 @AllArgsConstructor
-@XmlRootElement
 public class User {
     @Id
     @GeneratedValue
@@ -37,7 +40,8 @@ public class User {
     @Column(name = "user_password",length = 256, nullable = false)
     private String password;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<SkillAssessment> skillsAssessment = new ArrayList<SkillAssessment>();
 
     @Enumerated(EnumType.STRING)
