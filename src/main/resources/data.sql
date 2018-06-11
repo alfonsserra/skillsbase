c31a3106324064c76ad89be612409367d43d572c
alfonsserra <alfonsoserra@gmail.com>
2018-06-11 16:19:18 +0200

    Add more users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
-INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (1, 'Chandler', 'Sharat', 'user','$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 'USER');
-INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (2, 'Reinhold', 'Mark', 'admin','$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 'ADMIN');
-INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (3, 'Systelab', 'Systelab', 'Systelab','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'ADMIN');
+INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (1, 'Penalver', 'Aritz', 'apenalver','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'USER');
+INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (2, 'Orozco', 'Jorge', 'jorozco','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'ADMIN');
+INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (3, 'Galceran', 'Oscar', 'ogalceran','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'ADMIN');
+INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (4, 'Serra', 'Alfons', 'aserra','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'ADMIN');
+INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (5, 'Vinola', 'Carlos', 'cvinola','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'ADMIN');
+INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (6, 'Puig', 'Olga', 'opuig','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'ADMIN');
+INSERT INTO users (id, user_surname, user_name, user_login, user_password, user_role) VALUES (7, 'Legemaate', 'Alex', 'alegemaate','$2a$10$9wXu9hshOrtZ7RopythgF.XP93XbKtISBJMpz4PFHG4zv6QjTGBzq', 'ADMIN');
 
 INSERT INTO skill (id, text, type, comments, parent_skill) VALUES(0,'Top','category','',null);
 
